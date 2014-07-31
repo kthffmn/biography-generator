d47089466843a4e09a5a5bb49a5feb5b9e4a3ea5
Katie Hoffman <ktahoffman@gmail.com>
2014-07-31 14:02:04 -0400

    add logic to callback

--- app.rb
+++ app.rb
@@ -1,26 +1,25 @@
 require 'bundler'
+require 'open-uri'
+require 'json'
 Bundler.require
+require "dotenv"
+Dotenv.load
 require './lib/facebook_data.rb'
+
 require "pry"
 
 class App < Sinatra::Application
 
   get "/" do
     @root = ENV["ROOT_URL"]
-    binding.pry
     erb :index
   end
 
   get '/callback' do
-    redirect_uri = "#{ENV["ROOT_URL"]}/user"
+    redirect_uri = "#{ENV["ROOT_URL"]}/callback"
     code = params["code"]
-    binding.pry
-    redirect "https://graph.facebook.com/oauth/access_token?client_id=#{ENV["APP_ID"]}&redirect_uri=#{redirect_uri}&client_secret=#{ENV["APP_SECRET"]}&code=#{code}"
-  end
-
-  get '/user' do
-    binding.pry
-    token = params["access_token"]
+    user_json = JSON.load(open("https://graph.facebook.com/oauth/access_token?client_id=#{ENV["APP_ID"]}&redirect_uri=#{redirect_uri}&client_secret=#{ENV["APP_SECRET"]}&code=#{code}"))
+    token = user_json["access_token"]
     @data = FacebookData.new(token).main
     erb :result
   end
