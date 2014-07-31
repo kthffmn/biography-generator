b5c8cd59953a1ec345590714fdea92466320d70b
Katie Hoffman <ktahoffman@gmail.com>
2014-07-31 16:33:08 -0400

    Koala::Facebook::ServerError: type: OAuthException, code: 1, message: An unknown error has occurred. [HTTP 500]

--- app.rb
+++ app.rb
@@ -4,7 +4,6 @@ require 'bundler'
 Bundler.require
 require 'open-uri'
 require 'json'
-require 'pry'
 require './lib/facebook_data.rb'
 
 
@@ -19,12 +18,11 @@ class App < Sinatra::Application
     redirect_uri = "#{ENV["ROOT_URL"]}/callback"
     code = params["code"]
     html =  Nokogiri::HTML(open("https://graph.facebook.com/oauth/access_token?client_id=#{ENV["APP_ID"]}&redirect_uri=#{redirect_uri}&client_secret=#{ENV["APP_SECRET"]}&code=#{code}"))
-    token = html.search("p").children.text[13..-1]
+    token = html.search("p").children.text[13..-1].gsub(/&expires.+/, "")
     begin
       @data = FacebookData.new(token).main
     rescue
-      binding.pry
-      @data ||= "message: Malformed access token"
+      @data ||= "Koala::Facebook::ServerError: type: OAuthException, code: 1, message: An unknown error has occurred. [HTTP 500]"
     end
     erb :result
   end
