22ac16fa11009f68066c492bd0dac2045a7efcd7
Katie Hoffman <ktahoffman@gmail.com>
2014-07-31 16:49:40 -0400

    add new get method

--- app.rb
+++ app.rb
@@ -6,25 +6,19 @@ require 'open-uri'
 require 'json'
 require './lib/facebook_data.rb'
 
-
 class App < Sinatra::Application
   
   enable :sessions
 
   get "/" do
-    if session[:access_token].nil?
-      @facebook_url = "https://www.facebook.com/dialog/oauth?client_id=#{ENV["APP_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}"
-      erb :index
-    else
-      redirect '/result'
-    end
+    @facebook_url = "https://www.facebook.com/dialog/oauth?client_id=#{ENV["APP_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}"
+    erb :index
   end
 
   get '/callback' do
     code = params["code"]
     html =  Nokogiri::HTML(open("https://graph.facebook.com/oauth/access_token?client_id=#{ENV["APP_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}&client_secret=#{ENV["APP_SECRET"]}&code=#{code}"))
-    token = html.search("p").children.text[13..-1].gsub(/&expires.+/, "")
-    session[:access_token] = token
+    session[:access_token] = html.search("p").children.text[13..-1].gsub(/&expires.+/, "")
     redirect "/result"
   end
 
