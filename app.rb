8fc4629e72c4cb58343784dab7272f4e2d9ce8d7
Katie Hoffman <ktahoffman@gmail.com>
2014-08-04 12:20:35 -0400

    functional friend count

--- app.rb
+++ app.rb
@@ -11,23 +11,26 @@ class App < Sinatra::Application
   enable :sessions
 
   get "/" do
-    @facebook_url = "https://www.facebook.com/dialog/oauth?client_id=#{ENV["APP_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}"
+    @facebook_url = "https://www.facebook.com/dialog/oauth?client_id=#{ENV["APP_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}&scope=user_friends"
     erb :index
   end
 
   get '/callback' do
     code = params["code"]
-    html =  Nokogiri::HTML(open("https://graph.facebook.com/oauth/access_token?client_id=#{ENV["APP_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}&client_secret=#{ENV["APP_SECRET"]}&code=#{code}"))
-    session[:access_token] = html.search("p").children.text[13..-1].gsub(/&expires.+/, "")
+    url = "https://graph.facebook.com/oauth/access_token?client_id=#{ENV["APP_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}&client_secret=#{ENV["APP_SECRET"]}&code=#{code}"
+
+    params = {}
+    open(url).read.split("&").each do |param|
+      k,v = param.split("=")
+      params[k] = v
+    end
+
+    session[:access_token] = params["access_token"]
     redirect "/result"
   end
 
   get "/result" do
-    begin
-      @data = FacebookData.new(session[:access_token]).main
-    rescue
-      @data ||= "error occurred"
-    end
+    @data = FacebookData.new(session[:access_token]).data
     erb :result
   end
 
--- lib/facebook_data.rb
+++ lib/facebook_data.rb
@@ -1,3 +1,8 @@
+require 'koala'
+require 'json'
+require 'open-uri'
+require 'pry'
+
 class FacebookData
   attr_accessor :total_friends, :female_friends, :male_friends, 
                 :joiners, :lowest_id, :highest_id, :birthday_info
@@ -5,71 +10,79 @@ class FacebookData
 
   def initialize(token)
     @token = token
-    @graph = Koala::Facebook::API.new(token)
-    @birthday_info = []
+    @graph = Koala::Facebook::API.new(@token)
+    # @birthday_info = []
 
-    @joiners = {first_on_fb: {}, last_on_fb: {}}
+    # @joiners = {first_on_fb: {}, last_on_fb: {}}
 
-    @lowest_id = Float::INFINITY
-    @highest_id = 0
+    # @lowest_id = Float::INFINITY
+    # @highest_id = 0
 
-    @total_friends = 0
-    @male_friends = 0
-    @female_friends = 0
+    # @total_friends = 0
+    # @male_friends = 0
+    # @female_friends = 0
   end
 
-  def friends
-    graph.get_connections("me", "friends")
-  end
-
-  def fetch_data(id)
-    JSON.load(open("https://graph.facebook.com/user?id=#{id}&access_token=#{token}"))
-  end
-
-  def update_birthday_hash(data)
-    if data["birthday"]
-      birthday_info << {:first_name => data["first_name"], :last_name => data["last_name"], :id => data["id"], :birthday => data["birthday"] }
+  def friend_count
+    data = []
+    url = "https://graph.facebook.com/v2.0/me?fields=friends&access_token=#{token}"
+    open(url) do |f| 
+      f.each_line do |line| 
+        data << JSON.parse(line)
+      end
     end
+    data[0]["friends"]["summary"]["total_count"]
   end
 
-  def update_first_and_last_friends_on_fb(data)
-    id = data["id"].to_i
-    if id < lowest_id
-      joiners[:first_on_fb][:name] = data["name"]
-      joiners[:first_on_fb][:id] = id
-      self.lowest_id = id
-    end
-    if id > highest_id
-      joiners[:last_on_fb][:name] = data["name"]
-      joiners[:last_on_fb][:id] = id
-      self.highest_id = id
-    end
-  end
+  # def fetch_data(id)
+  #   JSON.load(open("https://graph.facebook.com/user?id=#{id}&access_token=#{token}"))
+  # end
 
-  def update_gender_ratios(data)
-    self.total_friends += 1
-    if data["gender"] == "male"
-      self.male_friends += 1
-    elsif data["gender"] == "female"
-      self.female_friends += 1
-    end
-  end
+  # def update_birthday_hash(data)
+  #   if data["birthday"]
+  #     birthday_info << {:first_name => data["first_name"], :last_name => data["last_name"], :id => data["id"], :birthday => data["birthday"] }
+  #   end
+  # end
 
-  def main
-    friends.each_with_index do |friend_hash, i|
-      data = fetch_data(friend_hash["id"])
-      update_birthday_hash(data)
-      update_first_and_last_friends_on_fb(data)
-      update_gender_ratios(data)
-    end
-    {:joiners => joiners, 
-     :friends => {
-      :total_friends => total_friends, 
-      :male_friends => male_friends,
-      :female_friends => female_friends
-      },
-    :friends_birthdays => birthday_info
-    }
-  end
+  # def update_first_and_last_friends_on_fb(data)
+  #   id = data["id"].to_i
+  #   if id < lowest_id
+  #     joiners[:first_on_fb][:name] = data["name"]
+  #     joiners[:first_on_fb][:id] = id
+  #     self.lowest_id = id
+  #   end
+  #   if id > highest_id
+  #     joiners[:last_on_fb][:name] = data["name"]
+  #     joiners[:last_on_fb][:id] = id
+  #     self.highest_id = id
+  #   end
+  # end
+
+  # def update_gender_ratios(data)
+  #   self.total_friends += 1
+  #   if data["gender"] == "male"
+  #     self.male_friends += 1
+  #   elsif data["gender"] == "female"
+  #     self.female_friends += 1
+  #   end
+  # end
+
+  # def main
+  #   friends.each_with_index do |friend_hash, i|
+  #     data = fetch_data(friend_hash["id"])
+  #     update_birthday_hash(data)
+  #     update_first_and_last_friends_on_fb(data)
+  #     update_gender_ratios(data)
+  #   end
+  #   {:joiners => joiners, 
+  #    :friends => {
+  #     :total_friends => total_friends, 
+  #     :male_friends => male_friends,
+  #     :female_friends => female_friends
+  #     },
+  #   :friends_birthdays => birthday_info
+  #   }
+  # end
 
 end
+
