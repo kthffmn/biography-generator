8b5691a5363d5be4829666ce1d1dfcad1e79ba86
Katie Hoffman <ktahoffman@gmail.com>
2014-08-06 14:31:33 -0400

    adds method to add value in case it doesn't exist

--- lib/facebook_data.rb
+++ lib/facebook_data.rb
@@ -14,11 +14,52 @@ class FacebookData
     @token = token
     @graph = Koala::Facebook::API.new(token)
     @data = {}
+    runner
+  end
+
+  def runner
     get_personal_info
     get_friend_count
     get_photos
     get_age
+    get_pronouns
     clean_data
+    add_missing_data
+  end
+
+  def suppliment_data(key)
+    if data[key] == nil
+      data[key] = "unknown #{key.to_s}"
+    end
+  end
+
+  def suppliment_college_data
+    if @data[:college] == nil
+      data[:college] = {:name => "unknown college", :year =>"unknown year"}
+    end
+  end
+
+  def add_missing_data
+    keys = [ :name, :hometown, :high_school, :age, :first_name, :location, :work, :total_friends, :email, :website]
+    keys.each do |key|
+      suppliment_data(key)
+    end
+    suppliment_college_data
+  end
+
+  def get_pronouns
+    if data[:gender]
+      if data[:gender] == "female"
+        data[:pronoun] = "she"
+        data[:possesive_pronoun] = "her"
+      elsif data[:gender] == "male" 
+        data[:pronoun] = "he"
+        data[:possesive_pronoun] = "his"
+      else
+        data[:pronoun] = "he/she"
+        data[:possesive_pronoun] = "his/her"
+      end
+    end
   end
 
   def clean_data
@@ -48,26 +89,30 @@ class FacebookData
     end
   end
 
-  def clean_work   
-    work = data[:work]
-    data[:work] = []
-    work.each do |edu|
-      data[:work] << edu["employer"]["name"]
+  def clean_work
+    if data[:work]
+      work = data[:work]
+      data[:work] = []
+      work.each do |edu|
+        data[:work] << edu["employer"]["name"]
+      end
+      employers = data[:work]
+      if employers.length > 2
+        employers[-1].insert(0, "and ")
+      end
+      temp_string = employers.join(", ")
+      data[:work] = temp_string
     end
-    employers = data[:work]
-    if employers.length > 2
-      employers[-1].insert(0, "and ")
-    end
-    temp_string = employers.join(", ")
-    data[:work] = temp_string
   end
 
   def get_age
-    bd = data[:birthday].split("/").collect {|s| s.to_i }
-    birthday = DateTime.new(bd[2], bd[0], bd[1])
-    now = Time.now.utc.to_date
-    age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
-    data[:age] = age
+    if data[:birthday]
+      bd = data[:birthday].split("/").collect {|s| s.to_i }
+      birthday = DateTime.new(bd[2], bd[0], bd[1])
+      now = Time.now.utc.to_date
+      age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+      data[:age] = age
+    end
   end
 
   def get_fb_data(url)
