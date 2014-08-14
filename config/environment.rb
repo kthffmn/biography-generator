773df08553d2a4b44d76bae45e5f80e0052a7909
Katie Hoffman <ktahoffman@gmail.com>
2014-08-14 10:16:58 -0400

    uses require relative in environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require './lib/facebook_data.rb'
-require './app.rb'
+require_relative '../lib/facebook_data.rb'
+require_relative '../app.rb'
